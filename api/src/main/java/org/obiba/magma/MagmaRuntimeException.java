b2af6df00c83f8e259477f9133689f0d5b89c4d5
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-11-17 20:17:44 +0000

    Added javadoc Removed serialization warning

--- api/src/main/java/org/obiba/magma/MagmaRuntimeException.java
+++ api/src/main/java/org/obiba/magma/MagmaRuntimeException.java
@@ -1,6 +1,11 @@
 package org.obiba.magma;
 
+/**
+ * Base class for all Magma runtime exceptions. Any exception thrown by Magma runtime should extend this class.
+ */
 public class MagmaRuntimeException extends RuntimeException {
+
+  private static final long serialVersionUID = -1825626210129821160L;
 
   public MagmaRuntimeException() {
     super();
