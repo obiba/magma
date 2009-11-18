5cd2c8903f356a797c813b8ec2f8b12ba31b88a1
Martin Boulanger <syrinx2004@gmail.com>
2009-11-18 22:04:39 +0000

    MAGMA-8 : Added classes CollectionWrapper and related classes.

---
+++ api/src/main/java/org/obiba/magma/support/AbstractCollectionWrapper.java
@@ -0,0 +1,67 @@
+package org.obiba.magma.support;
+
+import java.util.Set;
+
+import org.obiba.magma.Collection;
+import org.obiba.magma.Datasource;
+import org.obiba.magma.NoSuchVariableException;
+import org.obiba.magma.Occurrence;
+import org.obiba.magma.ValueSet;
+import org.obiba.magma.Variable;
+import org.obiba.magma.VariableEntity;
+import org.obiba.magma.VariableValueSource;
+
+public abstract class AbstractCollectionWrapper implements CollectionWrapper {
+
+  Collection collection;
+
+  @Override
+  public Datasource getDatasource() {
+    return collection.getDatasource();
+  }
+
+  @Override
+  public Set<VariableEntity> getEntities(String entityType) {
+    return collection.getEntities(entityType);
+  }
+
+  @Override
+  public Set<String> getEntityTypes() {
+    return collection.getEntityTypes();
+  }
+
+  @Override
+  public String getName() {
+    return collection.getName();
+  }
+
+  @Override
+  public VariableValueSource getVariableValueSource(String entityType, String variableName) throws NoSuchVariableException {
+    return collection.getVariableValueSource(entityType, variableName);
+  }
+
+  @Override
+  public Set<VariableValueSource> getVariableValueSources(String entityType) {
+    return collection.getVariableValueSources(entityType);
+  }
+
+  @Override
+  public Set<Variable> getVariables() {
+    return collection.getVariables();
+  }
+
+  @Override
+  public Set<Occurrence> loadOccurrences(ValueSet valueSet, Variable variable) {
+    return collection.loadOccurrences(valueSet, variable);
+  }
+
+  @Override
+  public ValueSet loadValueSet(VariableEntity entity) {
+    return collection.loadValueSet(entity);
+  }
+
+  @Override
+  public Collection getWrappedCollection() {
+    return collection;
+  }
+}
---
+++ api/src/main/java/org/obiba/magma/support/CollectionWrapper.java
@@ -0,0 +1,9 @@
+package org.obiba.magma.support;
+
+import org.obiba.magma.Collection;
+
+public interface CollectionWrapper extends Collection {
+
+  public Collection getWrappedCollection();
+
+}
