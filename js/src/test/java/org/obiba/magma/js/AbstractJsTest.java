69d8b417b0eac398e372cc075a604a233b0c59e6
Philippe Laflamme <philippe.laflamme@obiba.org>
2009-11-20 21:42:24 +0000

    Fixed unit tests

--- js/src/test/java/org/obiba/magma/js/AbstractJsTest.java
+++ js/src/test/java/org/obiba/magma/js/AbstractJsTest.java
@@ -2,25 +2,13 @@ package org.obiba.magma.js;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.ContextFactory;
 import org.obiba.magma.MagmaEngine;
 
 public abstract class AbstractJsTest {
 
-  @BeforeClass
-  public static void prepare() {
-    if(ContextFactory.hasExplicitGlobal() == false) {
-      ContextFactory.initGlobal(new MagmaContextFactory());
-      Context.enter().initStandardObjects();
-      Context.exit();
-    }
-  }
-
   @Before
   public void startYourEngine() {
-    newEngine();
+    newEngine().extend(new MagmaJsExtension());
   }
 
   @After
