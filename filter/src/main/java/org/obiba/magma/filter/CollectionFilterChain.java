a85e8ae248722f3bac4f34306c46356a4d376a0d
Tony DeBat <tony.debat@oicr.on.ca>
2009-12-04 20:29:49 +0000

    ONYX-1038 - Deserialisation of the purge configuration.

--- filter/src/main/java/org/obiba/magma/filter/CollectionFilterChain.java
+++ filter/src/main/java/org/obiba/magma/filter/CollectionFilterChain.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 public class CollectionFilterChain<T> implements FilterChain<T> {
 
+  @XStreamOmitField
   private String entityType;
 
   @XStreamImplicit
