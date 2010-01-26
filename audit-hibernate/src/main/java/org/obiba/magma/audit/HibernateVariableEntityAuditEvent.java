7f4f3ada002d33dcf7f49953e8baa9a658cb3dea
Martin Boulanger <syrinx2004@gmail.com>
2010-01-26 22:12:45 +0000

    OPAL-91 : Created the audit-hibernate project.  Added classes with incomplete implementation (to do).

---
+++ audit-hibernate/src/main/java/org/obiba/magma/audit/HibernateVariableEntityAuditEvent.java
@@ -0,0 +1,47 @@
+package org.obiba.magma.audit;
+import java.util.Date;
+import java.util.Map;
+
+import org.obiba.magma.Value;
+import org.obiba.magma.audit.VariableEntityAuditEvent;
+
+
+public class HibernateVariableEntityAuditEvent implements VariableEntityAuditEvent {
+
+	@Override
+	public String getDatasource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getDatetime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Value getDetailValue(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Value> getDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ audit-hibernate/src/main/java/org/obiba/magma/audit/HibernateVariableEntityAuditLogManager.java
@@ -0,0 +1,15 @@
+package org.obiba.magma.audit;
+import org.obiba.magma.VariableEntity;
+import org.obiba.magma.audit.VariableEntityAuditLog;
+import org.obiba.magma.audit.VariableEntityAuditLogManager;
+
+
+public class HibernateVariableEntityAuditLogManager implements VariableEntityAuditLogManager { 
+
+	@Override
+	public VariableEntityAuditLog getAuditLog(VariableEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
