c5f8f7d4d976bb7e8f247e051ede8b5d855abd45
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-01-26 14:17:18 +0000

    MAGMA-70

---
+++ api/src/main/java/org/obiba/magma/views/SelectClause.java
@@ -0,0 +1,17 @@
+package org.obiba.magma.views;
+
+import org.obiba.magma.Variable;
+
+/**
+ * Interface for abstracting how {@link Variable} instances are selected.
+ */
+public interface SelectClause {
+
+  /**
+   * Indicates whether the specified variable is selected by this clause.
+   * 
+   * @param variable a variable
+   * @return <code>true</code> if selected
+   */
+  public boolean select(Variable variable);
+}
