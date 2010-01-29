c64e9e5406767123153bc51e11ace94865cac708
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-01-29 20:17:54 +0000

    MAGMA-68:   - Implemented JavascriptClause.   - Unit tests.

---
+++ js/src/main/java/org/obiba/magma/js/views/JavascriptClause.java
@@ -0,0 +1,155 @@
+package org.obiba.magma.js.views;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextAction;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.EvaluatorException;
+import org.mozilla.javascript.Script;
+import org.mozilla.javascript.Scriptable;
+import org.obiba.magma.Initialisable;
+import org.obiba.magma.Value;
+import org.obiba.magma.ValueSet;
+import org.obiba.magma.Variable;
+import org.obiba.magma.js.MagmaContext;
+import org.obiba.magma.js.ScriptableValue;
+import org.obiba.magma.js.ScriptableVariable;
+import org.obiba.magma.type.BooleanType;
+import org.obiba.magma.views.SelectClause;
+import org.obiba.magma.views.WhereClause;
+
+public class JavascriptClause implements Initialisable, SelectClause, WhereClause {
+  //
+  // Instance Variables
+  //
+
+  private String scriptName = "customScript";
+
+  private String script;
+
+  private Script compiledScript;
+
+  //
+  // Constructors
+  //
+
+  public JavascriptClause(String script) {
+    this.script = script;
+    initialise();
+  }
+
+  //
+  // Initialisable Methods
+  //
+
+  @Override
+  public void initialise() throws EvaluatorException {
+    String script = getScript();
+    if(script == null) {
+      throw new NullPointerException("script cannot be null");
+    }
+    try {
+      compiledScript = (Script) ContextFactory.getGlobal().call(new ContextAction() {
+        @Override
+        public Object run(Context cx) {
+          return cx.compileString(getScript(), getScriptName(), 1, null);
+        }
+      });
+    } catch(EvaluatorException e) {
+      throw e;
+    }
+  }
+
+  //
+  // SelectClause Methods
+  //
+
+  @Override
+  public boolean select(final Variable variable) {
+    if(compiledScript == null) {
+      throw new IllegalStateException("script hasn't been compiled. Call initialise() before calling where().");
+    }
+
+    Value value = ((ScriptableValue) ContextFactory.getGlobal().call(new ContextAction() {
+      public Object run(Context ctx) {
+        MagmaContext context = MagmaContext.asMagmaContext(ctx);
+        // Don't pollute the global scope
+        Scriptable scope = new ScriptableVariable(context.newLocalScope(), variable);
+
+        Object value = compiledScript.exec(ctx, scope);
+
+        if(value instanceof Scriptable) {
+          return value;
+        }
+        return new ScriptableValue(scope, BooleanType.get().valueOf(value));
+      }
+    })).getValue();
+
+    return (Boolean) value.getValue();
+  }
+
+  //
+  // WhereClause Methods
+  //
+
+  @Override
+  public boolean where(final ValueSet valueSet) {
+    if(compiledScript == null) {
+      throw new IllegalStateException("script hasn't been compiled. Call initialise() before calling where().");
+    }
+
+    Value value = ((ScriptableValue) ContextFactory.getGlobal().call(new ContextAction() {
+      public Object run(Context ctx) {
+        MagmaContext context = MagmaContext.asMagmaContext(ctx);
+        // Don't pollute the global scope
+        Scriptable scope = context.newLocalScope();
+
+        enterContext(context, scope, valueSet);
+        Object value = compiledScript.exec(ctx, scope);
+        exitContext(context);
+
+        if(value instanceof Scriptable) {
+          return value;
+        }
+        return new ScriptableValue(scope, BooleanType.get().valueOf(value));
+      }
+    })).getValue();
+
+    return (Boolean) value.getValue();
+  }
+
+  //
+  // Methods
+  //
+
+  public String getScriptName() {
+    return scriptName;
+  }
+
+  public void setScriptName(String name) {
+    this.scriptName = name;
+  }
+
+  public String getScript() {
+    return script;
+  }
+
+  /**
+   * This method is invoked before evaluating the script. It provides a chance for derived classes to initialise values
+   * within the context. This method will add the current {@code ValueSet} as a {@code ThreadLocal} variable with
+   * {@code ValueSet#class} as its key. This allows other classes to have access to the current {@code ValueSet} during
+   * the script's execution.
+   * <p>
+   * Classes overriding this method must call their super class' method
+   * 
+   * @param ctx the current context
+   * @param scope the scope of execution of this script
+   * @param valueSet the current {@code ValueSet}
+   */
+  protected void enterContext(MagmaContext ctx, Scriptable scope, ValueSet valueSet) {
+    ctx.push(ValueSet.class, valueSet);
+  }
+
+  protected void exitContext(MagmaContext ctx) {
+    ctx.pop(ValueSet.class);
+  }
+}
