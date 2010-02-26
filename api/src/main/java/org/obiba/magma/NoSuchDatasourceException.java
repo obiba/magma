8af2a78f5dd32ad709d5c270cd17d8f38ac67d20
Philippe Laflamme <philippe.laflamme@obiba.org>
2010-02-26 15:48:05 +0000

    Added datasourceName attribute

--- api/src/main/java/org/obiba/magma/NoSuchDatasourceException.java
+++ api/src/main/java/org/obiba/magma/NoSuchDatasourceException.java
@@ -4,8 +4,15 @@ public class NoSuchDatasourceException extends MagmaRuntimeException {
 
   private static final long serialVersionUID = 5887330656285998606L;
 
+  private String datasourceName;
+
   public NoSuchDatasourceException(String datasourceName) {
     super("No datasource exists with the specified name '" + datasourceName + "'");
+    this.datasourceName = datasourceName;
+  }
+
+  public String getDatasourceName() {
+    return datasourceName;
   }
 
 }
