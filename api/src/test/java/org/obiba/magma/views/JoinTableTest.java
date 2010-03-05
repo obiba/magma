061437048e83d6de7bef768f32fe0e6cc1e048ad
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-03-05 21:05:20 +0000

    MAGMA-104:  - Added a unit test for JoinTable's getVariables() method.

--- api/src/test/java/org/obiba/magma/views/JoinTableTest.java
+++ api/src/test/java/org/obiba/magma/views/JoinTableTest.java
@@ -4,12 +4,17 @@ import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 import org.obiba.magma.ValueTable;
+import org.obiba.magma.Variable;
 
 public class JoinTableTest {
   //
@@ -51,8 +56,6 @@ public class JoinTableTest {
     tables.add(createMockTable("tableOne", PARTICIPANT_ENTITY_TYPE));
     tables.add(createMockTable("tableTwo", PARTICIPANT_ENTITY_TYPE));
 
-    replay(tables.toArray());
-
     new JoinTable(tables);
   }
 
@@ -63,8 +66,6 @@ public class JoinTableTest {
     tables.add(createMockTable("tableTwo", PARTICIPANT_ENTITY_TYPE));
     tables.add(createMockTable("tableThree", PARTICIPANT_ENTITY_TYPE));
 
-    replay(tables.toArray());
-
     new JoinTable(tables);
   }
 
@@ -74,8 +75,6 @@ public class JoinTableTest {
     tables.add(createMockTable("tableOne", PARTICIPANT_ENTITY_TYPE));
     tables.add(createMockTable("tableTwo", "someOtherType"));
 
-    replay(tables.toArray());
-
     new JoinTable(tables);
   }
 
@@ -84,8 +83,6 @@ public class JoinTableTest {
     List<ValueTable> tables = new ArrayList<ValueTable>();
     tables.add(createMockTable("tableOne", PARTICIPANT_ENTITY_TYPE));
     tables.add(createMockTable("tableTwo", PARTICIPANT_ENTITY_TYPE));
-
-    replay(tables.toArray());
 
     JoinTable joinTable = new JoinTable(tables);
     assertEquals(null, joinTable.getDatasource());
@@ -97,20 +94,54 @@ public class JoinTableTest {
     tables.add(createMockTable("tableOne", PARTICIPANT_ENTITY_TYPE));
     tables.add(createMockTable("tableTwo", PARTICIPANT_ENTITY_TYPE));
 
-    replay(tables.toArray());
-
     JoinTable joinTable = new JoinTable(tables);
     assertEquals("tableOne-tableTwo", joinTable.getName());
+  }
+
+  @Test
+  public void testJoinTableVariablesAreUnionOfIndividualTableVariables() {
+    List<ValueTable> tables = new ArrayList<ValueTable>();
+    tables.add(createMockTable("tableOne", PARTICIPANT_ENTITY_TYPE, "var1", "var2", "var3"));
+    tables.add(createMockTable("tableTwo", PARTICIPANT_ENTITY_TYPE, "var2", "var3", "var4"));
+    tables.add(createMockTable("tableThree", PARTICIPANT_ENTITY_TYPE, "var5", "var6"));
+
+    JoinTable joinTable = new JoinTable(tables);
+    Iterable<Variable> variables = joinTable.getVariables();
+    assertNotNull(variables);
+
+    List<String> variableNameList = new ArrayList<String>();
+    for(Variable variable : variables) {
+      variableNameList.add(variable.getName());
+    }
+
+    assertEquals(6, variableNameList.size());
+    for(String variableName : new String[] { "var1", "var2", "var3", "var4", "var5", "var6" }) {
+      assertTrue(variableName, variableNameList.contains(variableName));
+    }
   }
 
   //
   // Helper Methods
   //
 
-  private ValueTable createMockTable(String name, String entityType) {
+  private ValueTable createMockTable(String name, String entityType, String... variableNames) {
     ValueTable mockTable = createMock(ValueTable.class);
     expect(mockTable.getName()).andReturn(name).anyTimes();
     expect(mockTable.getEntityType()).andReturn(entityType).anyTimes();
+
+    if(variableNames.length != 0) {
+      Set<Variable> tableVariables = new HashSet<Variable>();
+      for(String variableName : variableNames) {
+        Variable mockVariable = createMock(Variable.class);
+        tableVariables.add(mockVariable);
+        expect(mockVariable.getName()).andReturn(variableName).anyTimes();
+        replay(mockVariable);
+      }
+
+      expect(mockTable.getVariables()).andReturn(tableVariables).anyTimes();
+    }
+
+    replay(mockTable);
 
     return mockTable;
   }
