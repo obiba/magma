8ccedb8dbca39475956ea4682c5004594f1ffc8d
Yannick Marcon <yannick.marcon@obiba.org>
2010-03-16 13:48:38 +0000

    Table name conevrting in excel datasource updated.

--- datasource-excel/src/main/java/org/obiba/magma/datasource/excel/support/NameConverter.java
+++ datasource-excel/src/main/java/org/obiba/magma/datasource/excel/support/NameConverter.java
@@ -3,7 +3,7 @@ package org.obiba.magma.datasource.excel.support;
 public class NameConverter {
 
   public static String toExcelName(String magmaName) {
-    return magmaName.replace('/', '-').replace('\\', '_').replace(':', '_').replace('*', '_').replace('?', '_');
+    return magmaName.replace('/', '|').replace('\\', '_').replace(':', '_').replace('*', '_').replace('?', '_');
   }
 
 }
