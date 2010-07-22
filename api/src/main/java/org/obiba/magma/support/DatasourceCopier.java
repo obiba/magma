19b5f3dadd2a16f3647b9ffe0fe95cefbf809a83
Yannick Marcon <yannick.marcon@obiba.org>
2010-07-22 13:15:14 +0000

    VariableCategoryTransformer moved from js project to api.

--- api/src/main/java/org/obiba/magma/support/DatasourceCopier.java
+++ api/src/main/java/org/obiba/magma/support/DatasourceCopier.java
@@ -7,6 +7,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import org.obiba.magma.Attribute;
+import org.obiba.magma.Category;
 import org.obiba.magma.Datasource;
 import org.obiba.magma.MagmaEngine;
 import org.obiba.magma.Value;
@@ -309,6 +311,42 @@ public class DatasourceCopier {
     public Variable transform(Variable variable) {
       return variable;
     }
+  }
+
+  /**
+   * Rename the categories of the given variable with their code if this one exists.
+   * 
+   */
+  static class VariableCategoryNameTransformer implements VariableTransformer {
+
+    @Override
+    public Variable transform(Variable variable) {
+      Variable.Builder builder = Variable.Builder.sameAs(variable, false);
+      for(Category category : variable.getCategories()) {
+        Category.Builder cBuilder = Category.Builder.sameAs(category);
+        // would be better to push the code as an 'alias' attribute and use this attribute to rename the category
+        String alias = null;
+        // if (category.hasAttribute("alias")) {
+        // alias = category.getAttribute("alias").getValue().toString();
+        // }
+        alias = category.getCode();
+        if(alias != null) {
+          cBuilder.name(alias);
+          // back up the old name as an alias
+          cBuilder.clearAttributes();
+          for(Attribute attr : category.getAttributes()) {
+            if(attr.getName().equals("alias")) {
+              cBuilder.addAttribute("alias", category.getName());
+            } else {
+              cBuilder.addAttribute(attr);
+            }
+          }
+        }
+        builder.addCategory(cBuilder.build());
+      }
+      return builder.build();
+    }
+
   }
 
   public interface MultiplexingStrategy {
