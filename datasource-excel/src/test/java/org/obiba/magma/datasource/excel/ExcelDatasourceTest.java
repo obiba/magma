ae4b47c055fadc6bcb2c4550e30111a0ce2e4fa8
Dennis Spathis <dennis.spathis@mail.mcgill.ca>
2010-11-02 02:05:44 +0000

    OPAL-238:   - Upgraded to POI version 3.7 to pick up fix for https://issues.apache.org/bugzilla/show_bug.cgi?id=48936.   - ExcelDatasource: Removed code that was added to workaround this bug.   - ExcelDatasourceTest: Updated OPAL-238 test (renamed as "test_OPAL_238_strings_can_be_written"). This test passes if the test strings can be written without error (which they can be now with POI 3.7; they could not be with POI 3.6).
    NOTE: This does NOT fix the problem described in an earlier comment, which occurs when creating an ExcelDatasource then disposing of it (by either removing it or by shutting down Opal). A crash occurs, which may be related to a bug in xml-beans: https://issues.apache.org/bugzilla/show_bug.cgi?id=49940.

--- datasource-excel/src/test/java/org/obiba/magma/datasource/excel/ExcelDatasourceTest.java
+++ datasource-excel/src/test/java/org/obiba/magma/datasource/excel/ExcelDatasourceTest.java
@@ -104,21 +104,21 @@ public class ExcelDatasourceTest {
       Assert.assertTrue(dpe.hasChildren());
       List<DatasourceParsingException> errors = dpe.getChildrenAsList();
       Assert.assertEquals(15, errors.size());
-	  assertDatasourceParsingException("DuplicateCategoryName", "[Categories, 4, Table1, Var1, C2]", errors.get(0));
-	  assertDatasourceParsingException("CategoryNameRequired", "[Categories, 5, Table1, Var1]", errors.get(1));
-	  assertDatasourceParsingException("DuplicateCategoryName", "[Categories, 7, Table1, Var2, C1]", errors.get(2));
-	  assertDatasourceParsingException("VariableNameRequired", "[Variables, 6, Table1]", errors.get(3));
-	  assertDatasourceParsingException("DuplicateVariableName", "[Variables, 7, Table1, Var1]", errors.get(4));
-	  assertDatasourceParsingException("VariableNameCannotContainColon", "[Variables, 8, Table1, Foo:Bar]", errors.get(5));
-	  assertDatasourceParsingException("UnknownValueType", "[Variables, 9, Table1, Var5, Numerical]", errors.get(6));
-	  assertDatasourceParsingException("UnidentifiedVariableName", "[Categories, 8, Table1, VarUnknown]", errors.get(7));
-	  assertDatasourceParsingException("CategoryVariableNameRequired", "[Categories, 9, Table1]", errors.get(8));
-	  assertDatasourceParsingException("CategoryVariableNameRequired", "[Categories, 10, Table1]", errors.get(9));
+      assertDatasourceParsingException("DuplicateCategoryName", "[Categories, 4, Table1, Var1, C2]", errors.get(0));
+      assertDatasourceParsingException("CategoryNameRequired", "[Categories, 5, Table1, Var1]", errors.get(1));
+      assertDatasourceParsingException("DuplicateCategoryName", "[Categories, 7, Table1, Var2, C1]", errors.get(2));
+      assertDatasourceParsingException("VariableNameRequired", "[Variables, 6, Table1]", errors.get(3));
+      assertDatasourceParsingException("DuplicateVariableName", "[Variables, 7, Table1, Var1]", errors.get(4));
+      assertDatasourceParsingException("VariableNameCannotContainColon", "[Variables, 8, Table1, Foo:Bar]", errors.get(5));
+      assertDatasourceParsingException("UnknownValueType", "[Variables, 9, Table1, Var5, Numerical]", errors.get(6));
+      assertDatasourceParsingException("UnidentifiedVariableName", "[Categories, 8, Table1, VarUnknown]", errors.get(7));
+      assertDatasourceParsingException("CategoryVariableNameRequired", "[Categories, 9, Table1]", errors.get(8));
+      assertDatasourceParsingException("CategoryVariableNameRequired", "[Categories, 10, Table1]", errors.get(9));
       assertDatasourceParsingException("DuplicateColumns", "[Table1, 1, Table1, Var2]", errors.get(10));
-      assertDatasourceParsingException("DuplicateColumns", "[Table1, 1, Table1, Var6]", errors.get(11));	
-	  assertDatasourceParsingException("VariableNameCannotContainColon", "[Table1, 1, Table1, Toto:Tata]", errors.get(12));
-      assertDatasourceParsingException("VariableNameRequired", "[Table1, 1, Table1]", errors.get(13));	  
-      assertDatasourceParsingException("VariableNameRequired", "[Variables, 10, Table2]", errors.get(14));	  	  
+      assertDatasourceParsingException("DuplicateColumns", "[Table1, 1, Table1, Var6]", errors.get(11));
+      assertDatasourceParsingException("VariableNameCannotContainColon", "[Table1, 1, Table1, Toto:Tata]", errors.get(12));
+      assertDatasourceParsingException("VariableNameRequired", "[Table1, 1, Table1]", errors.get(13));
+      assertDatasourceParsingException("VariableNameRequired", "[Variables, 10, Table2]", errors.get(14));
     }
   }
 
@@ -243,7 +243,7 @@ public class ExcelDatasourceTest {
   }
 
   @Test
-  public void testOPAL_238() throws IOException {
+  public void test_OPAL_238_strings_can_be_written() throws IOException {
     File tmp = createTempFile(".xlsx");
 
     Workbook w = new XSSFWorkbook();
@@ -253,17 +253,8 @@ public class ExcelDatasourceTest {
       s.createRow(i++).createCell(0).setCellValue(str);
     }
 
-    Throwable t = null;
-    try {
-      w.write(new FileOutputStream(tmp));
-      w = new XSSFWorkbook(new FileInputStream(tmp));
-      Assert.fail("If this test fails, it may mean that OPAL-238 is fixed. If so, reverse the assertions of this test.");
-    } catch(RuntimeException e) {
-      t = e;
-    } catch(AssertionError e) {
-      t = e;
-    }
-    Assert.assertNotNull("Due to a bug in POI, we expect to get an exception or an AssertionError (depends whether assertions are enabled).", t);
+    w.write(new FileOutputStream(tmp));
+    w = new XSSFWorkbook(new FileInputStream(tmp));
   }
 
   @Test
