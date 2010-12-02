661be97ea01ac47e8f1822878987c806ee13e34c
Yannick Marcon <yannick.marcon@obiba.org>
2010-12-02 17:25:46 +0000

    has and get DatasourceFactory added to MagmaEngineFactory.

--- api/src/main/java/org/obiba/magma/support/MagmaEngineFactory.java
+++ api/src/main/java/org/obiba/magma/support/MagmaEngineFactory.java
@@ -9,6 +9,7 @@ import org.obiba.magma.Datasource;
 import org.obiba.magma.DatasourceFactory;
 import org.obiba.magma.MagmaEngine;
 import org.obiba.magma.MagmaEngineExtension;
+import org.obiba.magma.NoSuchDatasourceException;
 
 import com.google.common.collect.Sets;
 
@@ -59,6 +60,24 @@ public class MagmaEngineFactory {
         break;
       }
     }
+  }
+
+  public boolean hasDatasourceFactory(String name) {
+    for(DatasourceFactory factory : factories) {
+      if(factory.getName().equals(name)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public DatasourceFactory getDatasourceFactory(String name) {
+    for(DatasourceFactory factory : factories) {
+      if(factory.getName().equals(name)) {
+        return factory;
+      }
+    }
+    throw new NoSuchDatasourceException("Datasource factory could not be found: " + name);
   }
 
   public MagmaEngineFactory withDatasource(Datasource datasource) {
