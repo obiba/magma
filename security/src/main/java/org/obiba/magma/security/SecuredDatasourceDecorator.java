e6ffacfb6d12591ee9f928e8e4dedc6e1db5106c
Philippe Laflamme <philippe.laflamme@obiba.org>
2011-02-01 18:59:25 +0000

    Added preconditions

--- security/src/main/java/org/obiba/magma/security/SecuredDatasourceDecorator.java
+++ security/src/main/java/org/obiba/magma/security/SecuredDatasourceDecorator.java
@@ -8,11 +8,13 @@ public class SecuredDatasourceDecorator implements Decorator<Datasource> {
   private final Authorizer authorizer;
 
   public SecuredDatasourceDecorator(Authorizer authorizer) {
+    if(authorizer == null) throw new IllegalArgumentException("authorizer cannot be null");
     this.authorizer = authorizer;
   }
 
   @Override
   public Datasource decorate(Datasource datasource) {
+    if(datasource == null) throw new IllegalArgumentException("datasource cannot be null");
     return new SecuredDatasource(authorizer, datasource);
   }
 
