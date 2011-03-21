3b8e45dc4e159ef4f762d4138b090c7a568dfa94
Yannick Marcon <yannick.marcon@obiba.org>
2011-03-21 15:13:22 +0000

    OPAL-1078 Register entities even if no variables are defined.

--- datasource-csv/src/main/java/org/obiba/magma/datasource/csv/CsvValueTable.java
+++ datasource-csv/src/main/java/org/obiba/magma/datasource/csv/CsvValueTable.java
@@ -238,7 +238,7 @@ public class CsvValueTable extends AbstractValueTable implements Initialisable, 
       line = dataReader.readNext();
       while(line != null) {
         count++;
-        if(line.length > 1) {
+        if(line.length > 0) {
           VariableEntity entity = new VariableEntityBean(entityType, line[0]);
           entityIndex.put(entity, lineIndex.get(count));
         }
