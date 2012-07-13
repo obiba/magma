72294dc821bc99814a75fe522eb9ab7c1df99d3b
Yannick Marcon <yannick.marcon@obiba.org>
2012-07-13 19:47:16 +0000

    MAGMA-187 $group method added.

--- js/src/main/java/org/obiba/magma/js/methods/GlobalMethods.java
+++ js/src/main/java/org/obiba/magma/js/methods/GlobalMethods.java
@@ -6,17 +6,21 @@ import java.util.Set;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Function;
+import org.mozilla.javascript.NativeObject;
 import org.mozilla.javascript.Scriptable;
 import org.obiba.magma.NoSuchValueSetException;
 import org.obiba.magma.Value;
+import org.obiba.magma.ValueSequence;
 import org.obiba.magma.ValueSet;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.ValueType;
+import org.obiba.magma.Variable;
 import org.obiba.magma.VariableEntity;
 import org.obiba.magma.VariableValueSource;
 import org.obiba.magma.VectorSource;
 import org.obiba.magma.js.JavascriptValueSource.VectorCache;
 import org.obiba.magma.js.MagmaContext;
+import org.obiba.magma.js.MagmaJsEvaluationRuntimeException;
 import org.obiba.magma.js.ScriptableValue;
 import org.obiba.magma.js.ScriptableVariable;
 import org.obiba.magma.support.MagmaEngineVariableResolver;
@@ -26,7 +30,10 @@ import org.obiba.magma.type.TextType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 
 public final class GlobalMethods extends AbstractGlobalMethodProvider {
 
@@ -35,7 +42,7 @@ public final class GlobalMethods extends AbstractGlobalMethodProvider {
   /**
    * Set of methods to be exposed as top-level methods (ones that can be invoked anywhere)
    */
-  private static final Set<String> GLOBAL_METHODS = ImmutableSet.of("$", "$join", "now", "log", "$var", "$id", "newValue");
+  private static final Set<String> GLOBAL_METHODS = ImmutableSet.of("$", "$join", "now", "log", "$var", "$id", "$group", "newValue");
 
   @Override
   protected Set<String> getExposedMethods() {
@@ -138,15 +145,7 @@ public final class GlobalMethods extends AbstractGlobalMethodProvider {
     MagmaContext context = MagmaContext.asMagmaContext(ctx);
     String name = (String) args[0];
 
-    MagmaEngineVariableResolver reference = MagmaEngineVariableResolver.valueOf(name);
-
-    VariableValueSource source = null;
-    if(context.has(ValueTable.class)) {
-      source = reference.resolveSource(context.peek(ValueTable.class));
-    } else {
-      source = reference.resolveSource();
-    }
-    return new ScriptableVariable(thisObj, source.getVariable());
+    return new ScriptableVariable(thisObj, variableFromContext(context, name));
   }
 
   /**
@@ -235,4 +234,155 @@ public final class GlobalMethods extends AbstractGlobalMethodProvider {
     return new ScriptableValue(thisObj, value, source.getVariable().getUnit());
   }
 
+  /**
+   * 
+   * @return
+   * @throws MagmaJsEvaluationRuntimeException
+   */
+  public static NativeObject $group(final Context ctx, Scriptable thisObj, Object[] args, final Function funObj) throws MagmaJsEvaluationRuntimeException {
+    if(args.length != 2) {
+      throw new IllegalArgumentException("$group() expects exactly two arguments: a variable name and a matching criteria (i.e. a value or a function).");
+    }
+    String name = (String) args[0];
+    Object criteria = args[1];
+
+    MagmaContext context = MagmaContext.asMagmaContext(ctx);
+
+    ScriptableValue sv = valueFromContext(context, thisObj, name);
+    NativeObject valueMap = new NativeObject();
+    Variable variable = variableFromContext(context, name);
+    ValueTable valueTable = valueTableFromContext(context);
+
+    if(sv.getValue().isNull() || sv.getValue().isSequence() == false) {
+      // just map itself
+      valueMap.put(variable.getName(), null, sv);
+      return valueMap;
+    }
+
+    Predicate<Value> predicate = getPredicate(ctx, sv.getParentScope(), thisObj, variable, criteria);
+    Iterable<Variable> variables = getVariablesFromOccurrenceGroup(valueTable, variable);
+
+    ValueSequence valueSequence = sv.getValue().asSequence();
+    int index = -1;
+    for(Value value : valueSequence.getValue()) {
+      index++;
+      if(predicate.apply(value)) {
+        valueMap.put(variable.getName(), valueMap, new ScriptableValue(thisObj, value));
+        // get variables of the same occurrence group and map values
+        mapValues(context, thisObj, valueMap, variables, index);
+        break;
+      }
+    }
+
+    return valueMap;
+  }
+
+  private static ValueTable valueTableFromContext(MagmaContext context) {
+    ValueTable valueTable = null;
+    if(context.has(ValueTable.class)) {
+      valueTable = context.peek(ValueTable.class);
+    }
+    return valueTable;
+  }
+
+  private static Variable variableFromContext(MagmaContext context, String name) {
+    MagmaEngineVariableResolver reference = MagmaEngineVariableResolver.valueOf(name);
+
+    VariableValueSource source = null;
+    if(context.has(ValueTable.class)) {
+      source = reference.resolveSource(context.peek(ValueTable.class));
+    } else {
+      source = reference.resolveSource();
+    }
+    return source.getVariable();
+  }
+
+  private static Predicate<Value> getPredicate(Context ctx, Scriptable scope, Scriptable thisObj, Variable variable, Object criteria) {
+    Predicate<Value> predicate;
+    if(criteria instanceof ScriptableValue) {
+      predicate = new ValuePredicate(((ScriptableValue) criteria).getValue());
+    } else if(criteria instanceof Function) {
+      predicate = new FunctionPredicate(ctx, scope, thisObj, (Function) criteria);
+    } else {
+      predicate = new ValuePredicate(variable.getValueType().valueOf(criteria));
+    }
+    return predicate;
+  }
+
+  private static Iterable<Variable> getVariablesFromOccurrenceGroup(ValueTable valueTable, final Variable variable) {
+    if(variable.getOccurrenceGroup() == null || valueTable == null) {
+      return ImmutableList.<Variable> builder().build();
+    }
+    return Iterables.filter(valueTable.getVariables(), new Predicate<Variable>() {
+      @Override
+      public boolean apply(Variable input) {
+        return variable.getOccurrenceGroup().equals(input.getOccurrenceGroup());
+      }
+    });
+  }
+
+  private static void mapValues(MagmaContext context, Scriptable thisObj, NativeObject valueMap, Iterable<Variable> variables, int index) {
+    if(index < 0) return;
+
+    for(Variable var : variables) {
+      ScriptableValue svalue = valueFromContext(context, thisObj, var.getName());
+      Value val = var.getValueType().nullValue();
+      if(svalue.getValue().isNull() == false) {
+        ValueSequence valSeq = svalue.getValue().asSequence();
+        if(index < valSeq.getSize()) {
+          val = valSeq.get(index);
+        }
+      }
+      valueMap.put(var.getName(), valueMap, new ScriptableValue(thisObj, val));
+    }
+  }
+
+  /**
+   * Predicate based on a function call.
+   */
+  private static final class FunctionPredicate implements Predicate<Value> {
+
+    private Context ctx;
+
+    private Scriptable scope;
+
+    private Scriptable thisObj;
+
+    private Function criteriaFunction;
+
+    public FunctionPredicate(Context ctx, Scriptable scope, Scriptable thisObj, Function criteria) {
+      super();
+      this.ctx = ctx;
+      this.scope = scope;
+      this.thisObj = thisObj;
+      this.criteriaFunction = criteria;
+    }
+
+    @Override
+    public boolean apply(Value input) {
+      Object rval = criteriaFunction.call(ctx, scope, thisObj, new ScriptableValue[] { new ScriptableValue(thisObj, input) });
+      if(rval instanceof ScriptableValue) {
+        rval = ((ScriptableValue) rval).getValue().getValue();
+      }
+      return rval == null ? false : (Boolean) rval;
+    }
+  }
+
+  /**
+   * Predicate based on the equality with a value.
+   */
+  private static final class ValuePredicate implements Predicate<Value> {
+
+    private Value criteriaValue;
+
+    public ValuePredicate(Value criteriaValue) {
+      super();
+      this.criteriaValue = criteriaValue;
+    }
+
+    @Override
+    public boolean apply(Value input) {
+      return input.equals(criteriaValue);
+    }
+  }
 }
