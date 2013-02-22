3b847b0aa4ba7202f039a16f416b3cf9127bdf2d
Ramin Haeri Azad <rhaeri@p3g.org>
2013-02-22 17:12:27 -0500

    MAGMA-198 - corrected the SpssDatasourceFactory to be used properly in opal

--- datasource-spss/src/test/java/org/obiba/magma/datasource/spss/DatasourceSpssTest.java
+++ datasource-spss/src/test/java/org/obiba/magma/datasource/spss/DatasourceSpssTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.obiba.magma.Datasource;
 import org.obiba.magma.MagmaRuntimeException;
 import org.obiba.magma.NoSuchVariableException;
 import org.obiba.magma.datasource.spss.support.SpssDatasourceFactory;
@@ -26,25 +27,24 @@ public class DatasourceSpssTest {
 
   private SpssDatasourceFactory dsFactory;
 
-  private static final String ROOT_FOLDER = "/home/rhaeri/projects/magma/datasource-spss/";
-
   @Before
   public void setUp() {
     dsFactory = new SpssDatasourceFactory();
+    dsFactory.setName(SpssDatasourceFactory.DEFAULT_DATASOURCE_NAME);
   }
 
   @Test
   public void createDatasourceWithValidFile() {
-    SpssDatasource ds = dsFactory
-        .create("spss", "src/test/resources/org/obiba/magma/datasource" + "/spss/DatabaseTest.sav");
-    Assert.assertEquals("spss", ds.getName());
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
+    Assert.assertEquals(SpssDatasourceFactory.DEFAULT_DATASOURCE_NAME, ds.getName());
   }
 
   @Test
   public void createDatasourceWithInvalidFile() {
-    SpssDatasource ds = dsFactory
-        .create("spss", "src/test/resources/org/obiba/magma/datasource" + "/spss/DatabaseTest.sa");
-    Assert.assertEquals("spss", ds.getName());
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
+    Assert.assertEquals(SpssDatasourceFactory.DEFAULT_DATASOURCE_NAME, ds.getName());
   }
 
   @Test
@@ -52,65 +52,72 @@ public class DatasourceSpssTest {
     File file1 = new File("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
     File file2 = new File("src/test/resources/org/obiba/magma/datasource/spss/HOP phase1d LifeLines.sav");
 
-    List<File> files = new ArrayList<File>();
-    files.add(file1);
-    files.add(file2);
+    dsFactory.addFile(file1);
+    dsFactory.addFile(file2);
 
-    SpssDatasource ds = dsFactory.create(files);
+    Datasource ds = dsFactory.create();
     Assert.assertEquals(SpssDatasourceFactory.DEFAULT_DATASOURCE_NAME, ds.getName());
   }
 
   @Test
   public void initilizeDatasourceWithValidFile() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
-    Assert.assertEquals("spss", ds.getName());
+    Assert.assertEquals(SpssDatasourceFactory.DEFAULT_DATASOURCE_NAME, ds.getName());
   }
 
   @Test(expected = MagmaRuntimeException.class)
   public void initilizeDatasourceWithInvalidFile() {
-      SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest");
+      dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest");
+      Datasource ds = dsFactory.create();
       ds.initialise();
   }
 
   @Test
   public void getValueTableDatasourceWithValidFile() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/HOP phase1d LifeLines.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/HOP phase1d LifeLines.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
     Assert.assertNotNull(ds.getValueTable("HOPphase1dLifeLines"));
   }
 
   @Test
   public void getValueTableDatasourceWithColonFileName() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/Database:Test.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/Database:Test.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
     Assert.assertNotNull(ds.getValueTable("DatabaseTest"));
   }
 
   @Test
   public void getVariableFromValueTable() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
     Assert.assertNotNull(ds.getValueTable("DatabaseTest").getVariable("race"));
   }
 
   @Test(expected = NoSuchVariableException.class)
   public void getVariableInUpperCaseFromValueTable() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
     Assert.assertNotNull(ds.getValueTable("DatabaseTest").getVariable("RACE"));
   }
 
   @Test(expected = NoSuchVariableException.class)
   public void getInvalidVariableFromValueTable() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
     Assert.assertNotNull(ds.getValueTable("DatabaseTest").getVariable("blabla"));
   }
 
   @Test
   public void getVariableTypeFromValueTable() {
-    SpssDatasource ds = dsFactory.create("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    dsFactory.setFile("src/test/resources/org/obiba/magma/datasource/spss/DatabaseTest.sav");
+    Datasource ds = dsFactory.create();
     ds.initialise();
     Assert.assertEquals(IntegerType.class, ds.getValueTable("DatabaseTest").getVariable("race").getValueType().getClass());
   }
