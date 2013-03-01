f3174fcff311a703498d9b8436dbb11cc85b7ae1
Ramin Haeri Azad <rhaeri@p3g.org>
2013-03-01 12:51:46 -0500

    Bug fix for Jenkins build

--- datasource-spss/src/test/java/org/obiba/magma/datasource/spss/support/SpssMagmaEngineTest.java
+++ datasource-spss/src/test/java/org/obiba/magma/datasource/spss/support/SpssMagmaEngineTest.java
@@ -1,18 +1,22 @@
 package org.obiba.magma.datasource.spss.support;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.obiba.magma.MagmaEngine;
 
 public abstract class SpssMagmaEngineTest {
 
-  @Before
-  public void startYourEngine() {
+  @BeforeClass
+  public static void startYourEngine() {
+    System.out.println("Starting engine");
     new MagmaEngine();
   }
 
-  @After
-  public void stopYourEngine() {
+  @AfterClass
+  public static void stopYourEngine() {
+    System.out.println("Shuttong down engine");
     MagmaEngine.get().shutdown();
   }
 }
