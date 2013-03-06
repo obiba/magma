d7a4303dd4ae837b885c09db25f03b11ca0f8c0c
Cedric Thiebault <cthiebault@p3g.org>
2013-03-06 10:51:21 -0500

    Add @Nullable/@Nonnull

--- api/src/main/java/org/obiba/magma/views/ViewManager.java
+++ api/src/main/java/org/obiba/magma/views/ViewManager.java
@@ -2,7 +2,7 @@ package org.obiba.magma.views;
 
 import java.util.Set;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 
 import org.obiba.magma.Datasource;
 import org.obiba.magma.Decorator;
@@ -10,15 +10,15 @@ import org.obiba.magma.Decorator;
 @SuppressWarnings("UnusedDeclaration")
 public interface ViewManager extends Decorator<Datasource> {
 
-  void addView(@Nullable String datasourceName, View view);
+  void addView(@Nonnull String datasourceName, @Nonnull View view);
 
-  void removeView(@Nullable String datasourceName, String viewName);
+  void removeView(@Nonnull String datasourceName, @Nonnull String viewName);
 
-  void removeAllViews(@Nullable String datasourceName);
+  void removeAllViews(@Nonnull String datasourceName);
 
-  boolean hasView(@Nullable String datasourceName, String viewName);
+  boolean hasView(@Nonnull String datasourceName, @Nonnull String viewName);
 
-  View getView(@Nullable String datasourceName, String viewName);
+  View getView(@Nonnull String datasourceName, @Nonnull String viewName);
 
-  void addViews(@Nullable String datasource, Set<View> views);
+  void addViews(@Nonnull String datasource, Set<View> views);
 }
