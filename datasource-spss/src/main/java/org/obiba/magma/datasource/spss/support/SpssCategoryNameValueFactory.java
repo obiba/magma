e189d9a92627e0d9a42707fbddf63df914cbcd66
Ramin Haeri Azad <rhaeri@p3g.org>
2013-03-08 15:14:10 -0500

    MAGMA-205 - Converted category so it matches the variable type

---
+++ datasource-spss/src/main/java/org/obiba/magma/datasource/spss/support/SpssCategoryNameValueFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2013 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.obiba.magma.datasource.spss.support;
+
+import org.obiba.magma.Value;
+import org.obiba.magma.ValueType;
+import org.opendatafoundation.data.spss.SPSSVariable;
+
+public class SpssCategoryNameValueFactory extends SpssValueFactory {
+
+  private final String category;
+
+  public SpssCategoryNameValueFactory(String category, int variableIndex, SPSSVariable spssVariable,
+      ValueType valueType) {
+    super(variableIndex, spssVariable, valueType);
+    this.category = category;
+  }
+
+  @Override
+  public Value create() {
+    try {
+      return createValue();
+    } catch(SpssInvalidCharacterException e) {
+      throw new SpssDatasourceParsingException("Failed to create variable", spssVariable.getName(), variableIndex,
+          "InvalidCategoryCharsetCharacter", variableIndex, spssVariable.getName());
+    }
+  }
+
+  @Override
+  protected String getValue() {
+    return category;
+  }
+}
