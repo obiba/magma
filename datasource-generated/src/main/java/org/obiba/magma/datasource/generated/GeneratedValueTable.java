bb3db429103778c1e9bb010f33d9c99a51966eb0
Cedric Thiebault <cthiebault@p3g.org>
2013-03-22 12:22:04 -0400

    Fix warnings

--- datasource-generated/src/main/java/org/obiba/magma/datasource/generated/GeneratedValueTable.java
+++ datasource-generated/src/main/java/org/obiba/magma/datasource/generated/GeneratedValueTable.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import org.apache.commons.math.random.JDKRandomGenerator;
 import org.apache.commons.math.random.RandomGenerator;
@@ -45,7 +46,7 @@ public class GeneratedValueTable implements ValueTable {
   @Nonnull
   private final Value timestamp;
 
-  public GeneratedValueTable(Datasource ds, Collection<Variable> dictionary, int entities) {
+  public GeneratedValueTable(@Nullable Datasource ds, Collection<Variable> dictionary, int entities) {
     this(ds, dictionary, entities, System.currentTimeMillis());
   }
 
