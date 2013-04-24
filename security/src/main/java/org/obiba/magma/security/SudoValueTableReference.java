59a1ec14da23b2e8bae01715022c2087c3c7828e
Yannick Marcon <yannick.marcon@obiba.org>
2013-04-24 16:17:00 -0400

    OPAL-1821 Dummy table is returned if resolver fails

--- security/src/main/java/org/obiba/magma/security/SudoValueTableReference.java
+++ security/src/main/java/org/obiba/magma/security/SudoValueTableReference.java
@@ -1,11 +1,20 @@
 package org.obiba.magma.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
+import org.obiba.magma.Datasource;
+import org.obiba.magma.MagmaEngine;
+import org.obiba.magma.NoSuchDatasourceException;
+import org.obiba.magma.NoSuchValueTableException;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.security.permissions.Permissions;
+import org.obiba.magma.support.StaticValueTable;
 import org.obiba.magma.support.ValueTableReference;
 import org.obiba.magma.support.ValueTableWrapper;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * An implementation of {@link ValueTableReference} that uses super user privileges to access the referenced table.
@@ -42,7 +51,21 @@ public class SudoValueTableReference extends ValueTableReference {
 
       @Override
       public ValueTable call() throws Exception {
-        return unwrap(getResolver().resolveTable());
+        try {
+          return unwrap(getResolver().resolveTable());
+        } catch(NoSuchValueTableException e1) {
+          return getDummyValueTable();
+        }
+      }
+
+      // OPAL-1821
+      private ValueTable getDummyValueTable() {
+        try {
+          Datasource ds = MagmaEngine.get().getDatasource(getResolver().getDatasourceName());
+          return new StaticValueTable(ds, getResolver().getTableName(), new ArrayList<String>(), "?");
+        } catch(NoSuchDatasourceException e2) {
+          return new StaticValueTable(getDatasource(), getResolver().getTableName(), new ArrayList<String>(), "?");
+        }
       }
     });
   }
