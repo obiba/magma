c79f271ccb43c093784a81f6884e6ac3633d76e4
Cedric Thiebault <cthiebault@p3g.org>
2013-05-16 15:56:14 -0400

    Add NoSuchDatasource when node is not found

--- magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/Neo4jValueTable.java
+++ magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/Neo4jValueTable.java
@@ -14,6 +14,7 @@ import javax.annotation.Nonnull;
 
 import org.obiba.magma.Datasource;
 import org.obiba.magma.NoSuchValueSetException;
+import org.obiba.magma.NoSuchValueTableException;
 import org.obiba.magma.Timestamps;
 import org.obiba.magma.Value;
 import org.obiba.magma.ValueSet;
@@ -67,7 +68,11 @@ public class Neo4jValueTable extends AbstractValueTable {
   }
 
   @Nonnull
-  private ValueTableNode getNode() {
-    return getDatasource().getNeo4jTemplate().findOne(graphId, ValueTableNode.class);
+  private ValueTableNode getNode() throws NoSuchValueTableException {
+    try {
+      return getDatasource().getNeo4jTemplate().findOne(graphId, ValueTableNode.class);
+    } catch(Exception e) {
+      throw new NoSuchValueTableException(getName());
+    }
   }
 }
