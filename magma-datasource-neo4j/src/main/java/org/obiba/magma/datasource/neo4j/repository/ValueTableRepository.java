2529687daf0480572038f91119d812eff2fbed20
Cedric Thiebault <cthiebault@p3g.org>
2013-05-17 16:11:11 -0400

    Update Copyrights

--- magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/repository/ValueTableRepository.java
+++ magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/repository/ValueTableRepository.java
@@ -11,13 +11,16 @@ package org.obiba.magma.datasource.neo4j.repository;
 
 import org.obiba.magma.datasource.neo4j.domain.DatasourceNode;
 import org.obiba.magma.datasource.neo4j.domain.ValueTableNode;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.neo4j.repository.RelationshipOperationsRepository;
 
 public interface ValueTableRepository
     extends GraphRepository<ValueTableNode>, RelationshipOperationsRepository<ValueTableNode> {
 
-  //TODO
+  @Query("start user=node:User({0}) match user-[r:RATED]->movie return movie order by r.stars desc limit 10")
   ValueTableNode findByName(DatasourceNode datasource, String name);
+
+  ValueTableNode findByName(String datasourceName, String name);
 
 }
