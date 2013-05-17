79decfc5f47ab9533118306b90cf67594b7a5da7
Cedric Thiebault <cthiebault@p3g.org>
2013-05-17 16:00:40 -0400

    Test domain nodes

--- magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/Neo4jValueTable.java
+++ magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/Neo4jValueTable.java
@@ -10,9 +10,15 @@
 
 package org.obiba.magma.datasource.neo4j;
 
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import javax.annotation.Nonnull;
 
+import org.obiba.core.util.TimedExecution;
 import org.obiba.magma.Datasource;
+import org.obiba.magma.Initialisable;
 import org.obiba.magma.NoSuchValueSetException;
 import org.obiba.magma.NoSuchValueTableException;
 import org.obiba.magma.Timestamps;
@@ -20,11 +26,16 @@ import org.obiba.magma.Value;
 import org.obiba.magma.ValueSet;
 import org.obiba.magma.VariableEntity;
 import org.obiba.magma.datasource.neo4j.converter.Neo4jMarshallingContext;
+import org.obiba.magma.datasource.neo4j.domain.ValueSetNode;
 import org.obiba.magma.datasource.neo4j.domain.ValueTableNode;
+import org.obiba.magma.datasource.neo4j.domain.VariableEntityNode;
 import org.obiba.magma.support.AbstractValueTable;
+import org.obiba.magma.support.AbstractVariableEntityProvider;
+import org.obiba.magma.support.VariableEntityBean;
 import org.obiba.magma.type.DateTimeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
 
 public class Neo4jValueTable extends AbstractValueTable {
 
@@ -36,6 +47,14 @@ public class Neo4jValueTable extends AbstractValueTable {
     super(datasource, valueTableNode.getName());
     graphId = valueTableNode.getGraphId();
     log.debug("ValueTable: {}, graphId: {}", valueTableNode.getName(), graphId);
+    setVariableEntityProvider(new Neo4jVariableEntityProvider(valueTableNode.getEntityType()));
+  }
+
+  @Override
+  public void initialise() {
+    super.initialise();
+    ((Initialisable) getVariableEntityProvider()).initialise();
+    cacheVariables();
   }
 
   @Override
@@ -69,15 +88,59 @@ public class Neo4jValueTable extends AbstractValueTable {
   }
 
   @Nonnull
-  private ValueTableNode getNode() throws NoSuchValueTableException {
+  ValueTableNode getNode() throws NoSuchValueTableException {
     try {
-      return getDatasource().getNeo4jTemplate().findOne(graphId, ValueTableNode.class);
+      return getNeo4jTemplate().findOne(graphId, ValueTableNode.class);
     } catch(Exception e) {
       throw new NoSuchValueTableException(getName());
     }
   }
 
+  private Neo4jTemplate getNeo4jTemplate() {
+    return getDatasource().getNeo4jTemplate();
+  }
+
   Neo4jMarshallingContext createContext() {
     return getDatasource().createContext(getNode());
+  }
+
+  private void cacheVariables() {
+    log.debug("Populating variable cache for table {}", getName());
+    TimedExecution timedExecution = new TimedExecution().start();
+    addVariableValueSources(new Neo4jVariableValueSourceFactory(this).createSources());
+    log.debug("Populating variable cache for {}: {} variables loaded in {}", getName(), getSources().size(),
+        timedExecution.end().formatExecutionTime());
+  }
+
+  public class Neo4jVariableEntityProvider extends AbstractVariableEntityProvider implements Initialisable {
+
+    private final Set<VariableEntity> entities = new LinkedHashSet<VariableEntity>();
+
+    public Neo4jVariableEntityProvider(String entityType) {
+      super(entityType);
+    }
+
+    @Override
+    public void initialise() {
+      log.debug("Populating entity cache for table {}", getName());
+      // get the variable entities that have a value set in the table
+
+      TimedExecution timedExecution = new TimedExecution().start();
+
+      ValueTableNode tableNode = getNode();
+//      getDatasource().getNeo4jTemplate().fetch(tableNode.getValueSets());
+      for(ValueSetNode valueSetNode : tableNode.getValueSets()) {
+        VariableEntityNode variableEntityNode = valueSetNode.getVariableEntity();
+        getNeo4jTemplate().fetch(variableEntityNode);
+        entities.add(new VariableEntityBean(variableEntityNode.getType(), variableEntityNode.getIdentifier()));
+      }
+      log.debug("Populating entity cache for {}: {} entities loaded in {}", getName(), entities.size(),
+          timedExecution.end().formatExecutionTime());
+    }
+
+    @Override
+    public Set<VariableEntity> getVariableEntities() {
+      return Collections.unmodifiableSet(entities);
+    }
   }
 }
