f66a0eb10cb1db888eb7b981371efcbfd44e9f62
Cedric Thiebault <cthiebault@p3g.org>
2013-05-22 09:35:53 -0400

    Add ValueRepository

---
+++ magma-datasource-neo4j/src/main/java/org/obiba/magma/datasource/neo4j/repository/ValueRepository.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2013 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.obiba.magma.datasource.neo4j.repository;
+
+import org.obiba.magma.datasource.neo4j.domain.ValueNode;
+import org.obiba.magma.datasource.neo4j.domain.ValueSetNode;
+import org.obiba.magma.datasource.neo4j.domain.VariableNode;
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.neo4j.repository.RelationshipOperationsRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ValueRepository extends GraphRepository<ValueNode>, RelationshipOperationsRepository<ValueNode> {
+
+  @Query("start variable=node({variable}), valueSet=node({valueSet}) " +
+      "match (variable)-[:VARIABLE_HAS_VALUE_SET_VALUES]->(valueSetValue), " +
+      "(valueSet)-[:VALUE_SET_HAS_VALUE_SET_VALUES]->(valueSetValue)-[:HAS_VALUE]->(value)" +
+      "return value")
+  ValueNode find(@Param("variable") VariableNode variable, @Param("valueSet") ValueSetNode valueSet);
+
+}
