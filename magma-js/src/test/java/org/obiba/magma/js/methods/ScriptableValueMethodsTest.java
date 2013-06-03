c46178b88f23070d946947663821d81d50fb7536
Cedric Thiebault <cthiebault@p3g.org>
2013-06-03 09:07:14 -0400

    Add test for asSequence (cherry picked from commit 8e1a68f)

--- magma-js/src/test/java/org/obiba/magma/js/methods/ScriptableValueMethodsTest.java
+++ magma-js/src/test/java/org/obiba/magma/js/methods/ScriptableValueMethodsTest.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.Locale;
 
 import org.junit.Test;
-import org.mozilla.javascript.Context;
+import org.obiba.magma.ValueSequence;
 import org.obiba.magma.js.AbstractJsTest;
 import org.obiba.magma.js.ScriptableValue;
 import org.obiba.magma.type.BinaryType;
@@ -15,6 +15,12 @@ import org.obiba.magma.type.LocaleType;
 import org.obiba.magma.type.TextType;
 
 import junit.framework.Assert;
+
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mozilla.javascript.Context.getCurrentContext;
 
 public class ScriptableValueMethodsTest extends AbstractJsTest {
 
@@ -29,49 +35,43 @@ public class ScriptableValueMethodsTest extends AbstractJsTest {
   @Test
   public void testTypeForTextValue() {
     ScriptableValue textValue = newValue(TextType.get().valueOf("Text value"));
-    ScriptableValue valueType = ScriptableValueMethods
-        .type(Context.getCurrentContext(), textValue, new Object[] { }, null);
-    Assert.assertEquals("text", valueType.getValue().getValue());
+    ScriptableValue valueType = ScriptableValueMethods.type(getCurrentContext(), textValue, new Object[] { }, null);
+    assertEquals("text", valueType.getValue().getValue());
   }
 
   @Test
   public void testTypeForBooleanValue() {
     ScriptableValue booleanValue = newValue(BooleanType.get().valueOf(true));
-    ScriptableValue valueType = ScriptableValueMethods
-        .type(Context.getCurrentContext(), booleanValue, new Object[] { }, null);
-    Assert.assertEquals("boolean", valueType.getValue().getValue());
+    ScriptableValue valueType = ScriptableValueMethods.type(getCurrentContext(), booleanValue, new Object[] { }, null);
+    assertEquals("boolean", valueType.getValue().getValue());
   }
 
   @Test
   public void testTypeForDateValue() {
     ScriptableValue dateValue = newValue(DateTimeType.get().valueOf(new Date()));
-    ScriptableValue valueType = ScriptableValueMethods
-        .type(Context.getCurrentContext(), dateValue, new Object[] { }, null);
-    Assert.assertEquals("datetime", valueType.getValue().getValue());
+    ScriptableValue valueType = ScriptableValueMethods.type(getCurrentContext(), dateValue, new Object[] { }, null);
+    assertEquals("datetime", valueType.getValue().getValue());
   }
 
   @Test
   public void testTypeForLocaleValue() {
     ScriptableValue localeValue = newValue(LocaleType.get().valueOf(Locale.CANADA_FRENCH));
-    ScriptableValue valueType = ScriptableValueMethods
-        .type(Context.getCurrentContext(), localeValue, new Object[] { }, null);
-    Assert.assertEquals("locale", valueType.getValue().getValue());
+    ScriptableValue valueType = ScriptableValueMethods.type(getCurrentContext(), localeValue, new Object[] { }, null);
+    assertEquals("locale", valueType.getValue().getValue());
   }
 
   @Test
   public void testTypeForBinaryValue() {
     ScriptableValue binaryContent = newValue(BinaryType.get().valueOf("binary content"));
-    ScriptableValue valueType = ScriptableValueMethods
-        .type(Context.getCurrentContext(), binaryContent, new Object[] { }, null);
-    Assert.assertEquals("binary", valueType.getValue().getValue());
+    ScriptableValue valueType = ScriptableValueMethods.type(getCurrentContext(), binaryContent, new Object[] { }, null);
+    assertEquals("binary", valueType.getValue().getValue());
   }
 
   @Test
   public void testTypeForIntegerValue() {
     ScriptableValue integer = newValue(IntegerType.get().valueOf(1));
-    ScriptableValue valueType = ScriptableValueMethods
-        .type(Context.getCurrentContext(), integer, new Object[] { }, null);
-    Assert.assertEquals("integer", valueType.getValue().getValue());
+    ScriptableValue valueType = ScriptableValueMethods.type(getCurrentContext(), integer, new Object[] { }, null);
+    assertEquals("integer", valueType.getValue().getValue());
   }
 
   @Test
@@ -79,7 +79,7 @@ public class ScriptableValueMethodsTest extends AbstractJsTest {
     Date currentDateTime = new Date();
     ScriptableValue dateValue = newValue(DateTimeType.get().valueOf(currentDateTime));
     ScriptableValue convertedValue = ScriptableValueMethods
-        .type(Context.getCurrentContext(), dateValue, new Object[] { "text" }, null);
+        .type(getCurrentContext(), dateValue, new Object[] { "text" }, null);
     Assert.assertSame(TextType.class, convertedValue.getValueType().getClass());
   }
 
@@ -87,52 +87,75 @@ public class ScriptableValueMethodsTest extends AbstractJsTest {
   public void convertDateToInteger() {
     Date currentDateTime = new Date();
     ScriptableValue dateValue = newValue(DateTimeType.get().valueOf(currentDateTime));
-    ScriptableValueMethods.type(Context.getCurrentContext(), dateValue, new Object[] { "integer" }, null);
+    ScriptableValueMethods.type(getCurrentContext(), dateValue, new Object[] { "integer" }, null);
   }
 
   @Test
   public void convertBinaryToText() {
     ScriptableValue binaryContent = newValue(BinaryType.get().valueOf("binary content"));
     ScriptableValue convertedValue = ScriptableValueMethods
-        .type(Context.getCurrentContext(), binaryContent, new Object[] { "text" }, null);
+        .type(getCurrentContext(), binaryContent, new Object[] { "text" }, null);
     Assert.assertSame(TextType.class, convertedValue.getValueType().getClass());
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void convertBinaryToDate() {
     ScriptableValue binaryContent = newValue(BinaryType.get().valueOf("binary content"));
-    ScriptableValueMethods.type(Context.getCurrentContext(), binaryContent, new Object[] { "datetime" }, null);
+    ScriptableValueMethods.type(getCurrentContext(), binaryContent, new Object[] { "datetime" }, null);
   }
 
   @Test
   public void convertIntegerToText() {
     ScriptableValue integerValue = newValue(IntegerType.get().valueOf(12));
     ScriptableValue convertedValue = ScriptableValueMethods
-        .type(Context.getCurrentContext(), integerValue, new Object[] { "text" }, null);
+        .type(getCurrentContext(), integerValue, new Object[] { "text" }, null);
     Assert.assertSame(IntegerType.class, integerValue.getValueType().getClass());
-    Assert.assertEquals("12", convertedValue.getValue().getValue());
+    assertEquals("12", convertedValue.getValue().getValue());
   }
 
   @Test
   public void lengthOfIntegerValue() {
     ScriptableValue value = newValue(IntegerType.get().valueOf(123));
-    ScriptableValue length = ScriptableValueMethods.length(Context.getCurrentContext(), value, new Object[] { }, null);
-    Assert.assertEquals(3l, length.getValue().getValue());
+    ScriptableValue length = ScriptableValueMethods.length(getCurrentContext(), value, new Object[] { }, null);
+    assertEquals(3l, length.getValue().getValue());
   }
 
   @Test
   public void lengthOfTextValue() {
     ScriptableValue value = newValue(TextType.get().valueOf("abcd"));
-    ScriptableValue length = ScriptableValueMethods.length(Context.getCurrentContext(), value, new Object[] { }, null);
-    Assert.assertEquals(4l, length.getValue().getValue());
+    ScriptableValue length = ScriptableValueMethods.length(getCurrentContext(), value, new Object[] { }, null);
+    assertEquals(4l, length.getValue().getValue());
   }
 
   @Test
   public void lengthOfTextValueSequence() {
     ScriptableValue value = newValue(TextType.get().sequenceOf("abcd,efg"));
-    ScriptableValue length = ScriptableValueMethods.length(Context.getCurrentContext(), value, new Object[] { }, null);
-    Assert.assertEquals(2l, length.getValue().asSequence().getSize());
-    Assert.assertEquals(4l, length.getValue().asSequence().getValues().get(0).getValue());
-    Assert.assertEquals(3l, length.getValue().asSequence().getValues().get(1).getValue());
+    ScriptableValue length = ScriptableValueMethods.length(getCurrentContext(), value, new Object[] { }, null);
+    ValueSequence sequence = length.getValue().asSequence();
+    assertEquals(2l, sequence.getSize());
+    assertEquals(4l, sequence.getValues().get(0).getValue());
+    assertEquals(3l, sequence.getValues().get(1).getValue());
+  }
+
+  @Test
+  public void asSequence() {
+    ScriptableValue value = newValue(IntegerType.get().valueOf(1));
+    ScriptableValue sv = ScriptableValueMethods.asSequence(getMagmaContext(), value, null, null);
+    assertThat(sv.getValue(), notNullValue());
+    assertThat(sv.getValue().isNull(), is(false));
+    assertThat(sv.getValue().isSequence(), is(true));
+
+    ValueSequence sequence = sv.getValue().asSequence();
+    assertThat(sequence.getSize(), is(1));
+    assertThat((Long) sequence.getValues().get(0).getValue(), is(1l));
+  }
+
+  @Test
+  public void asSequence_for_sequence() {
+    ScriptableValue value = newValue(TextType.get().sequenceOf("abcd,efg"));
+    ScriptableValue sv = ScriptableValueMethods.asSequence(getMagmaContext(), value, null, null);
+    assertThat(sv.getValue(), notNullValue());
+    assertThat(sv.getValue().isNull(), is(false));
+    assertThat(sv.getValue(), is(value.getValue()));
   }
 }
