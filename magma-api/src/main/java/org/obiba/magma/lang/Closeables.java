0d27158884267b37f43b868677f5a43d58662329
Yannick Marcon <yannick.marcon@obiba.org>
2013-06-13 12:01:28 -0400

    Added closing of a list of closeables.

--- magma-api/src/main/java/org/obiba/magma/lang/Closeables.java
+++ magma-api/src/main/java/org/obiba/magma/lang/Closeables.java
@@ -11,18 +11,29 @@ public final class Closeables {
   }
 
   /**
-   * Calls {@code Closeable#close} on a non-null {@code closable} and eats {@code IOException} exceptions (any {@code
+   * Calls {@code Closeable#close} on a non-null {@code Closable} and eats {@code IOException} exceptions (any {@code
    * RuntimeException} will still be thrown).
    *
    * @param closable the {@code Closeable} instance to close
    */
   public static void closeQuietly(@Nullable Closeable closable) {
-    if(closable != null) {
-      try {
-        closable.close();
-      } catch(IOException e) {
-        // Ignored
-      }
+    if(closable == null) return;
+    try {
+      closable.close();
+    } catch(IOException e) {
+      // Ignored
+    }
+  }
+
+  /**
+   * Close without exception the list of {@code Closable}.
+   * @param closables
+   */
+  public static void closeQuietly(@Nullable Closeable... closables) {
+    if(closables == null) return;
+
+    for(Closeable closeable : closables) {
+      closeQuietly(closeable);
     }
   }
 
