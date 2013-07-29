47ee62df9876e139410614ed4a17898890ca8eb9
Yannick Marcon <yannick.marcon@obiba.org>
2013-07-29 15:05:59 -0400

    MongoDB read/write geo values.

---
+++ magma-datasource-mongodb/src/main/java/org/obiba/magma/datasource/mongodb/MongoDBValueSet.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2013 OBiBa. All rights reserved.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.obiba.magma.datasource.mongodb;
+
+import javax.annotation.Nonnull;
+
+import org.obiba.magma.NoSuchValueSetException;
+import org.obiba.magma.Timestamps;
+import org.obiba.magma.Value;
+import org.obiba.magma.ValueSet;
+import org.obiba.magma.ValueTable;
+import org.obiba.magma.Variable;
+import org.obiba.magma.VariableEntity;
+import org.obiba.magma.datasource.mongodb.converter.ValueConverter;
+import org.obiba.magma.type.DateTimeType;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+
+class MongoDBValueSet implements ValueSet {
+
+  private final MongoDBValueTable valueTable;
+
+  private final VariableEntity entity;
+
+  private DBObject object;
+
+  public MongoDBValueSet(MongoDBValueTable valueTable, VariableEntity entity) {
+    this.valueTable = valueTable;
+    this.entity = entity;
+  }
+
+  @Override
+  public ValueTable getValueTable() {
+    return valueTable;
+  }
+
+  @Override
+  public VariableEntity getVariableEntity() {
+    return entity;
+  }
+
+  public Value getValue(Variable variable) {
+    loadDBObject();
+    return ValueConverter.unmarshall(variable, object);
+  }
+
+  @Override
+  public Timestamps getTimestamps() {
+    return new Timestamps() {
+      @Nonnull
+      @Override
+      public Value getLastUpdate() {
+        return getTimestamp("updated");
+      }
+
+      @Nonnull
+      @Override
+      public Value getCreated() {
+        return getTimestamp("created");
+      }
+
+      private Value getTimestamp(String key) {
+        loadDBObject();
+        DBObject timestamps = (DBObject) object.get(MongoDBValueTable.TIMESTAMPS_FIELD);
+        return ValueConverter.unmarshall(DateTimeType.get(), timestamps.get(key));
+      }
+    };
+  }
+
+  private void loadDBObject() {
+    if(object == null) {
+      DBObject template = BasicDBObjectBuilder.start("_id", entity.getIdentifier()).get();
+      object = valueTable.getValueSetCollection().findOne(template);
+      if(object == null) {
+        throw new NoSuchValueSetException(valueTable, entity);
+      }
+    }
+  }
+
+}
