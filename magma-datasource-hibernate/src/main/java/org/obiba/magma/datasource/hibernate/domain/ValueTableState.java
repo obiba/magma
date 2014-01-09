8cd690dd4d6505d173844c68469ce843d0f8b555
Cedric Thiebault <cthiebault@p3g.org>
2014-01-09 10:27:49 -0500

    Use @OrderColumn instead of @IndexColumn and need to upgrade Opal DBs

--- magma-datasource-hibernate/src/main/java/org/obiba/magma/datasource/hibernate/domain/ValueTableState.java
+++ magma-datasource-hibernate/src/main/java/org/obiba/magma/datasource/hibernate/domain/ValueTableState.java
@@ -19,10 +19,9 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
-import org.hibernate.annotations.IndexColumn;
 
 @Entity
 @Table(name = "value_table", uniqueConstraints = @UniqueConstraint(columnNames = { "datasource_id", "name" }))
@@ -42,7 +41,7 @@ public class ValueTableState extends AbstractTimestampedEntity {
   private DatasourceState datasource;
 
   @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true, mappedBy = "valueTable")
-  @IndexColumn(name = "variable_index", nullable = false)
+  @OrderColumn(name = "variable_index")
   private List<VariableState> variables;
 
   public ValueTableState() { }
