2daa1d1050c29b074ce42505d663b7d1e3fc62d0
Cedric Thiebault <cthiebault@p3g.org>
2014-01-14 15:17:25 -0500

    Benchmark vector reads

--- magma-datasource-mongodb/src/test/java/org/obiba/magma/datasource/mongodb/MongoDBDatasourceBenchmark.java
+++ magma-datasource-mongodb/src/test/java/org/obiba/magma/datasource/mongodb/MongoDBDatasourceBenchmark.java
@@ -10,6 +10,7 @@ import org.obiba.magma.MagmaEngine;
 import org.obiba.magma.ValueSet;
 import org.obiba.magma.ValueTable;
 import org.obiba.magma.Variable;
+import org.obiba.magma.VariableEntity;
 import org.obiba.magma.datasource.fs.FsDatasource;
 import org.obiba.magma.support.DatasourceCopier;
 import org.obiba.magma.support.Initialisables;
@@ -18,6 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Sets;
 import com.mongodb.MongoClient;
 
 import static com.google.common.collect.Iterables.size;
@@ -32,7 +34,7 @@ public class MongoDBDatasourceBenchmark {
 
   private static final String ONYX_DATA_ZIP = "20-onyx-data.zip";
 
-  private static final String FNAC_ZIP = "FNAC.zip";
+//  private static final String FNAC_ZIP = "FNAC.zip";
 
   private final Stopwatch stopwatch = Stopwatch.createUnstarted();
 
@@ -60,9 +62,12 @@ public class MongoDBDatasourceBenchmark {
     importData(source, datasource);
     Set<ValueTable> valueTables = getValueTables(datasource);
     for(ValueTable valueTable : valueTables) {
+      benchmarkLog.info("{}", valueTable.getName());
       Iterable<Variable> variables = getVariables(valueTable);
       Iterable<ValueSet> valueSets = getValueSets(valueTable);
-      readAllValues(valueTable, variables, valueSets);
+      Set<VariableEntity> entities = getEntities(valueTable);
+      readValues(valueTable, variables, valueSets);
+      readVectors(valueTable, variables, entities);
     }
   }
 
@@ -76,25 +81,41 @@ public class MongoDBDatasourceBenchmark {
   private Iterable<Variable> getVariables(ValueTable valueTable) {
     stopwatch.reset().start();
     Iterable<Variable> variables = valueTable.getVariables();
-    benchmarkLog.info("{}: load {} variables in {}", valueTable.getName(), size(variables), stopwatch);
+    benchmarkLog.info("  load {} variables in {}", size(variables), stopwatch);
     return variables;
   }
 
   private Iterable<ValueSet> getValueSets(ValueTable valueTable) {
     stopwatch.reset().start();
     Iterable<ValueSet> valueSets = valueTable.getValueSets();
-    benchmarkLog.info("{}: load {} valueSets in {}", valueTable.getName(), size(valueSets), stopwatch);
+    benchmarkLog.info("  load {} valueSets in {}", size(valueSets), stopwatch);
     return valueSets;
   }
 
-  private void readAllValues(ValueTable valueTable, Iterable<Variable> variables, Iterable<ValueSet> valueSets) {
+  private Set<VariableEntity> getEntities(ValueTable valueTable) {
+    stopwatch.reset().start();
+    Set<VariableEntity> entities = valueTable.getVariableEntities();
+    benchmarkLog.info("  load {} entities in {}", size(entities), stopwatch);
+    return entities;
+  }
+
+  private void readValues(ValueTable valueTable, Iterable<Variable> variables, Iterable<ValueSet> valueSets) {
     stopwatch.reset().start();
     for(Variable variable : variables) {
       for(ValueSet valueSet : valueSets) {
         valueTable.getValue(variable, valueSet);
       }
     }
-    benchmarkLog.info("{}: load values in {}", valueTable.getName(), stopwatch);
+    benchmarkLog.info("  load values in {}", stopwatch);
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  private void readVectors(ValueTable valueTable, Iterable<Variable> variables, Iterable<VariableEntity> entities) {
+    stopwatch.reset().start();
+    for(Variable variable : variables) {
+      valueTable.getVariableValueSource(variable.getName()).asVectorSource().getValues(Sets.newTreeSet(entities));
+    }
+    benchmarkLog.info("  load vectors in {}", stopwatch);
   }
 
   private void importData(Datasource source, Datasource dest) throws IOException {
