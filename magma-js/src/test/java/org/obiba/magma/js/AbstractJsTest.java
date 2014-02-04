d075e1e3c6e4cba4f6000c6beb94e33f1879a8f9
Cedric Thiebault <cthiebault@p3g.org>
2014-02-04 10:37:12 -0500

    Use MongoDB with generated values for $ and $this integration tests

--- magma-js/src/test/java/org/obiba/magma/js/AbstractJsTest.java
+++ magma-js/src/test/java/org/obiba/magma/js/AbstractJsTest.java
@@ -19,13 +19,13 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public abstract class AbstractJsTest {
 
   @Before
-  public void startYourEngine() {
+  public void before() {
     newEngine().extend(new MagmaJsExtension());
     Context.enter();
   }
 
   @After
-  public void stopYourEngine() {
+  public void after() {
     Context.exit();
     MagmaEngine.get().shutdown();
   }
