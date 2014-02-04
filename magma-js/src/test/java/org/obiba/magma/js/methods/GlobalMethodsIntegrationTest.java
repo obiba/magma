81f7bb78f5179c2608f890b92e9b4df86a5c3375
Cedric Thiebault <cthiebault@p3g.org>
2014-02-04 12:42:52 -0500

    Add test for vector

--- magma-js/src/test/java/org/obiba/magma/js/methods/GlobalMethodsIntegrationTest.java
+++ magma-js/src/test/java/org/obiba/magma/js/methods/GlobalMethodsIntegrationTest.java
@@ -7,6 +7,7 @@ import java.util.TreeSet;
 
 import org.junit.Assume;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mozilla.javascript.WrappedException;
 import org.obiba.magma.Datasource;
@@ -61,7 +62,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
 
   private static final String VARIABLE_HEIGHT = "height";
 
-  private final ViewManager viewManager = new DefaultViewManagerImpl(new MemoryViewPersistenceStrategy());
+  private ViewManager viewManager;
 
   @Before
   @Override
@@ -69,6 +70,8 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
     super.before();
     // run test only if MongoDB is running
     Assume.assumeTrue(setupMongoDB());
+
+    viewManager = new DefaultViewManagerImpl(new MemoryViewPersistenceStrategy());
   }
 
   @Override
@@ -160,19 +163,17 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
     View view = viewManager.getView(DATASOURCE, "view");
     VectorSource viewVectorSource = view.getVariableValueSource("weight_in_lbs").asVectorSource();
     assertThat(viewVectorSource).isNotNull();
+    int i = 0;
     //noinspection ConstantConditions
-    List<Value> viewValues = Lists.newArrayList(viewVectorSource.getValues(new TreeSet<>(view.getVariableEntities())));
-
-    for(int i = 0; i < viewValues.size(); i++) {
-      Long kg = (Long) tableValues.get(i).getValue();
-      Long lbs = (Long) viewValues.get(i).getValue();
+    for(Value viewValue : viewVectorSource.getValues(new TreeSet<>(view.getVariableEntities()))) {
+      Long kg = (Long) tableValues.get(i++).getValue();
+      Long lbs = (Long) viewValue.getValue();
       assertThat(lbs).isEqualTo((long) (kg * 2.2));
     }
-
   }
 
   @Test
-  public void test_$_dependencies() throws Exception {
+  public void test_$_value_set_with_valid_dependencies() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -190,13 +191,37 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       variableWriter.writeVariable(varD);
       variableWriter.writeVariable(varE);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
-    View view = viewManager.getView("ds", "view");
+    View view = viewManager.getView(DATASOURCE, "view");
     for(ValueSet valueSet : view.getValueSets()) {
       view.getValue(varA, valueSet);
     }
+  }
 
+  @Test
+  @Ignore
+  public void test_$_vector_with_valid_dependencies() throws Exception {
+    Datasource datasource = getTestDatasource();
+    ValueTable table = datasource.getValueTable(TABLE);
+
+    Variable varA = createIntVariable("A", "$('ds.view:B') + $('ds.view:C')");
+    Variable varB = createIntVariable("B", "10");
+    Variable varC = createIntVariable("C", "$('ds.view:D') * 10");
+    Variable varD = createIntVariable("D", "$('ds.view:E') + 5");
+    Variable varE = createIntVariable("E", "5");
+
+    View viewTemplate = View.Builder.newView("view", table).list(new VariablesClause()).build();
+    try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
+      variableWriter.writeVariable(varA);
+      variableWriter.writeVariable(varB);
+      variableWriter.writeVariable(varC);
+      variableWriter.writeVariable(varD);
+      variableWriter.writeVariable(varE);
+    }
+    viewManager.addView(DATASOURCE, viewTemplate, null);
+
+    View view = viewManager.getView(DATASOURCE, "view");
     VectorSource vectorSource = view.getVariableValueSource("A").asVectorSource();
     assertThat(vectorSource).isNotNull();
     //noinspection ConstantConditions
@@ -206,7 +231,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
   }
 
   @Test
-  public void test_$_self_reference() throws Exception {
+  public void test_$_value_set_with_self_reference() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -215,9 +240,9 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
     try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
       variableWriter.writeVariable(circular);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
-    View view = viewManager.getView("ds", "view");
+    View view = viewManager.getView(DATASOURCE, "view");
     try {
       for(ValueSet valueSet : view.getValueSets()) {
         view.getValue(circular, valueSet);
@@ -229,7 +254,21 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       assertThat(cause).isInstanceOf(CircularVariableDependencyRuntimeException.class);
       assertThat(((CircularVariableDependencyRuntimeException) cause).getVariableRef()).isEqualTo("ds.view:circular");
     }
+  }
 
+  @Test
+  public void test_$_vector_with_self_reference() throws Exception {
+    Datasource datasource = getTestDatasource();
+    ValueTable table = datasource.getValueTable(TABLE);
+
+    Variable circular = createIntVariable("circular", "$('ds.view:circular')");
+    View viewTemplate = View.Builder.newView("view", table).list(new VariablesClause()).build();
+    try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
+      variableWriter.writeVariable(circular);
+    }
+    viewManager.addView(DATASOURCE, viewTemplate, null);
+
+    View view = viewManager.getView(DATASOURCE, "view");
     try {
       VectorSource vectorSource = view.getVariableValueSource("circular").asVectorSource();
       assertThat(vectorSource).isNotNull();
@@ -247,7 +286,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
   }
 
   @Test
-  public void test_$_valueSets_with_circular_dependencies() throws Exception {
+  public void test_$_value_set_with_circular_dependencies() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -265,9 +304,9 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       variableWriter.writeVariable(varD);
       variableWriter.writeVariable(varE);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
-    View view = viewManager.getView("ds", "view");
+    View view = viewManager.getView(DATASOURCE, "view");
     try {
       for(ValueSet valueSet : view.getValueSets()) {
         view.getValue(varA, valueSet);
@@ -283,6 +322,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
   }
 
   @Test
+  @Ignore
   public void test_$_vector_with_circular_dependencies() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
@@ -301,9 +341,9 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       variableWriter.writeVariable(varD);
       variableWriter.writeVariable(varE);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
-    View view = viewManager.getView("ds", "view");
+    View view = viewManager.getView(DATASOURCE, "view");
     try {
       VectorSource vectorSource = view.getVariableValueSource("A").asVectorSource();
       assertThat(vectorSource).isNotNull();
@@ -321,7 +361,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
   }
 
   @Test
-  public void test_$this() throws Exception {
+  public void test_$this_value_set() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -335,7 +375,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       variableWriter.writeVariable(kgWeightRef);
       variableWriter.writeVariable(lbsWeight);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
     List<Long> tableValues = new ArrayList<>();
     for(ValueSet valueSet : table.getValueSets()) {
@@ -343,7 +383,7 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
     }
 
     List<Long> viewValues = new ArrayList<>();
-    View view = viewManager.getView("ds", "view");
+    View view = viewManager.getView(DATASOURCE, "view");
     for(ValueSet valueSet : view.getValueSets()) {
       viewValues.add((Long) view.getValue(lbsWeight, valueSet).getValue());
     }
@@ -355,7 +395,40 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
   }
 
   @Test
-  public void test_$this_self_reference() throws Exception {
+  public void test_$this_vector() throws Exception {
+    Datasource datasource = getTestDatasource();
+    ValueTable table = datasource.getValueTable(TABLE);
+
+    Variable kgWeightRef = createIntVariable("weight_in_kg", "$('ds.table:weight')");
+    Variable lbsWeight = createIntVariable("weight_in_lbs", "$this('weight_in_kg') * 2.2");
+
+    View viewTemplate = View.Builder.newView("view", table).list(new VariablesClause()).build();
+    try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
+      variableWriter.writeVariable(kgWeightRef);
+      variableWriter.writeVariable(lbsWeight);
+    }
+    viewManager.addView(DATASOURCE, viewTemplate, null);
+
+    VectorSource tableVectorSource = table.getVariableValueSource("weight").asVectorSource();
+    assertThat(tableVectorSource).isNotNull();
+    //noinspection ConstantConditions
+    List<Value> tableValues = Lists
+        .newArrayList(tableVectorSource.getValues(new TreeSet<>(table.getVariableEntities())));
+
+    View view = viewManager.getView(DATASOURCE, "view");
+    VectorSource viewVectorSource = view.getVariableValueSource("weight_in_lbs").asVectorSource();
+    assertThat(viewVectorSource).isNotNull();
+    int i = 0;
+    //noinspection ConstantConditions
+    for(Value viewValue : viewVectorSource.getValues(new TreeSet<>(view.getVariableEntities()))) {
+      Long kg = (Long) tableValues.get(i++).getValue();
+      Long lbs = (Long) viewValue.getValue();
+      assertThat(lbs).isEqualTo((long) (kg * 2.2));
+    }
+  }
+
+  @Test
+  public void test_$this_value_set_with_self_reference() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -364,10 +437,10 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
     try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
       variableWriter.writeVariable(circular);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
     try {
-      View view = viewManager.getView("ds", "view");
+      View view = viewManager.getView(DATASOURCE, "view");
       for(ValueSet valueSet : view.getValueSets()) {
         view.getValue(circular, valueSet);
       }
@@ -381,7 +454,36 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
   }
 
   @Test
-  public void test_inner_$this() throws Exception {
+  public void test_$this_vector_with_self_reference() throws Exception {
+    Datasource datasource = getTestDatasource();
+    ValueTable table = datasource.getValueTable(TABLE);
+
+    Variable circular = createIntVariable("circular", "$this('circular')");
+    View viewTemplate = View.Builder.newView("view", table).list(new VariablesClause()).build();
+    try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
+      variableWriter.writeVariable(circular);
+    }
+    viewManager.addView(DATASOURCE, viewTemplate, null);
+
+    View view = viewManager.getView(DATASOURCE, "view");
+    try {
+      VectorSource vectorSource = view.getVariableValueSource("circular").asVectorSource();
+      assertThat(vectorSource).isNotNull();
+      //noinspection ConstantConditions
+      for(Value value : vectorSource.getValues(new TreeSet<>(view.getVariableEntities()))) {
+        value.getValue();
+      }
+      fail("Should throw WrappedException");
+    } catch(WrappedException e) {
+      Throwable cause = e.getCause();
+      assertThat(cause).isNotNull();
+      assertThat(cause).isInstanceOf(CircularVariableDependencyRuntimeException.class);
+      assertThat(((CircularVariableDependencyRuntimeException) cause).getVariableRef()).isEqualTo("ds.view:circular");
+    }
+  }
+
+  @Test
+  public void test_$this_value_set_with_inner_$this() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -397,16 +499,48 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       variableWriter.writeVariable(varC);
       variableWriter.writeVariable(varD);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
-    View view = viewManager.getView("ds", "view");
+    View view = viewManager.getView(DATASOURCE, "view");
     for(ValueSet valueSet : view.getValueSets()) {
       view.getValue(varA, valueSet);
     }
   }
 
   @Test
-  public void test_inner_$this_with_circular_dependencies() throws Exception {
+  public void test_$this_vector_with_inner_$this() throws Exception {
+    Datasource datasource = getTestDatasource();
+    ValueTable table = datasource.getValueTable(TABLE);
+
+    Variable varA = createIntVariable("A", "$this('B') + $this('C') + $this('D')");
+    Variable varB = createIntVariable("B", "1");
+    Variable varC = createIntVariable("C", "10");
+    Variable varD = createIntVariable("D", "$this('B') * 5");
+
+    View viewTemplate = View.Builder.newView("view", table).list(new VariablesClause()).build();
+    try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
+      variableWriter.writeVariable(varA);
+      variableWriter.writeVariable(varB);
+      variableWriter.writeVariable(varC);
+      variableWriter.writeVariable(varD);
+    }
+    viewManager.addView(DATASOURCE, viewTemplate, null);
+
+    View view = viewManager.getView(DATASOURCE, "view");
+    for(ValueSet valueSet : view.getValueSets()) {
+      view.getValue(varA, valueSet);
+    }
+
+    VectorSource vectorSource = view.getVariableValueSource("A").asVectorSource();
+    assertThat(vectorSource).isNotNull();
+    //noinspection ConstantConditions
+    for(Value value : vectorSource.getValues(new TreeSet<>(view.getVariableEntities()))) {
+      value.getValue();
+    }
+  }
+
+  @Test
+  public void test_$this_value_set_with_circular_dependencies() throws Exception {
     Datasource datasource = getTestDatasource();
     ValueTable table = datasource.getValueTable(TABLE);
 
@@ -422,12 +556,49 @@ public class GlobalMethodsIntegrationTest extends AbstractJsTest {
       variableWriter.writeVariable(varC);
       variableWriter.writeVariable(varD);
     }
-    viewManager.addView("ds", viewTemplate, null);
+    viewManager.addView(DATASOURCE, viewTemplate, null);
 
+    View view = viewManager.getView(DATASOURCE, "view");
     try {
-      View view = viewManager.getView("ds", "view");
       for(ValueSet valueSet : view.getValueSets()) {
         view.getValue(varA, valueSet);
+      }
+      fail("Should throw WrappedException");
+    } catch(WrappedException e) {
+      Throwable cause = e.getCause();
+      assertThat(cause).isNotNull();
+      assertThat(cause).isInstanceOf(CircularVariableDependencyRuntimeException.class);
+      assertThat(((CircularVariableDependencyRuntimeException) cause).getVariableRef()).isEqualTo("ds.view:A");
+    }
+  }
+
+  @Test
+  @Ignore
+  public void test_$this_vector_with_circular_dependencies() throws Exception {
+    Datasource datasource = getTestDatasource();
+    ValueTable table = datasource.getValueTable(TABLE);
+
+    Variable varA = createIntVariable("A", "$this('B') + $this('C') + $this('D')");
+    Variable varB = createIntVariable("B", "1");
+    Variable varC = createIntVariable("C", "10");
+    Variable varD = createIntVariable("D", "$this('A') * 5");
+
+    View viewTemplate = View.Builder.newView("view", table).list(new VariablesClause()).build();
+    try(ValueTableWriter.VariableWriter variableWriter = viewTemplate.getListClause().createWriter()) {
+      variableWriter.writeVariable(varA);
+      variableWriter.writeVariable(varB);
+      variableWriter.writeVariable(varC);
+      variableWriter.writeVariable(varD);
+    }
+    viewManager.addView(DATASOURCE, viewTemplate, null);
+
+    View view = viewManager.getView(DATASOURCE, "view");
+    try {
+      VectorSource vectorSource = view.getVariableValueSource("A").asVectorSource();
+      assertThat(vectorSource).isNotNull();
+      //noinspection ConstantConditions
+      for(Value value : vectorSource.getValues(new TreeSet<>(view.getVariableEntities()))) {
+        value.getValue();
       }
       fail("Should throw WrappedException");
     } catch(WrappedException e) {
