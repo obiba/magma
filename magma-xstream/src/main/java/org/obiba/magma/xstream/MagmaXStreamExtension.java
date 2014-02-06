f2a79d96e939957b7414f96ffd5cb1c59ac925e5
Cedric Thiebault <cthiebault@p3g.org>
2014-02-06 11:56:19 -0500

    Fix PMD warnings

--- magma-xstream/src/main/java/org/obiba/magma/xstream/MagmaXStreamExtension.java
+++ magma-xstream/src/main/java/org/obiba/magma/xstream/MagmaXStreamExtension.java
@@ -12,6 +12,7 @@ public class MagmaXStreamExtension implements MagmaEngineExtension {
 
   private final transient XStreamFactory currentFactory = new DefaultXStreamFactory();
 
+  @edu.umd.cs.findbugs.annotations.SuppressWarnings("SE_TRANSIENT_FIELD_NOT_RESTORED")
   private final transient Map<String, ? extends XStreamFactory> compatibleFactories = ImmutableMap
       .of("1", currentFactory);
 
