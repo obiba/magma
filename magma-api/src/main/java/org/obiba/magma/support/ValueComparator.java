7c702505a281f531e9afd1bf61bdf24611829800
Yannick Marcon <yannick.marcon@obiba.org>
2014-04-16 10:09:29 -0400

    Handle error when comparing with MagmaDate

--- magma-api/src/main/java/org/obiba/magma/support/ValueComparator.java
+++ magma-api/src/main/java/org/obiba/magma/support/ValueComparator.java
@@ -2,6 +2,7 @@ package org.obiba.magma.support;
 
 import java.util.Comparator;
 
+import org.obiba.magma.MagmaDate;
 import org.obiba.magma.Value;
 
 @SuppressWarnings("Singleton")
@@ -18,9 +19,16 @@ public class ValueComparator implements Comparator<Value> {
     if(o1.isNull() && o2.isNull()) return 0;
     if(o1.isNull()) return -1;
     if(o2.isNull()) return 1;
-    Comparable l1 = (Comparable) o1.getValue();
-    Comparable l2 = (Comparable) o2.getValue();
+    Comparable l1 = (Comparable) normalizeValue(o1.getValue());
+    Comparable l2 = (Comparable) normalizeValue(o2.getValue());
     return l1 == l2 ? 0 : l1.compareTo(l2);
+  }
+
+  private Object normalizeValue(Object o) {
+    if (o instanceof MagmaDate) {
+      return ((MagmaDate)o).asDate();
+    }
+    return o;
   }
 
 }
