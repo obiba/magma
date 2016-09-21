bf3b4921c862d0b734ee19c6569623487ef7b8c6
Yannick Marcon <yannick.marcon@obiba.org>
2016-09-21 12:19:15 +0200

    OPAL-2805 Fix default value for tilestamps in jdbc datasource

--- magma-datasource-jdbc/src/main/java/org/obiba/magma/datasource/jdbc/JdbcDatasource.java
+++ magma-datasource-jdbc/src/main/java/org/obiba/magma/datasource/jdbc/JdbcDatasource.java
@@ -77,7 +77,7 @@ public class JdbcDatasource extends AbstractDatasource {
 
   private static final String TYPE = "jdbc";
 
-  public static final String EPOCH = ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC")).format(DateTimeFormatter.ISO_INSTANT);
+  public static final String EPOCH = ZonedDateTime.ofInstant(Instant.EPOCH, ZoneId.of("UTC")).plusDays(1).format(DateTimeFormatter.ISO_INSTANT);
 
   private final JdbcTemplate jdbcTemplate;
 
