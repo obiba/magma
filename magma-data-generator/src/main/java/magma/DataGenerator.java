898513036cceab49d96eb9b2c5739de879eb7c28
Yannick Marcon <yannick.marcon@obiba.org>
2016-10-12 22:46:27 +0200

    Updated copyright

--- magma-data-generator/src/main/java/magma/DataGenerator.java
+++ magma-data-generator/src/main/java/magma/DataGenerator.java
@@ -1,11 +1,12 @@
-/*******************************************************************************
- * Copyright (c) 2012 OBiBa. All rights reserved.
+/*
+ * Copyright (c) 2016 OBiBa. All rights reserved.
  *
- * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0.
  *
- * You should have received a copy of the GNU General Public License along with this program. If not, see
- * <http://www.gnu.org/licenses/>.
- ******************************************************************************/
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package magma;
 
 import java.io.File;
