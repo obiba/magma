a185ce43303f3222c6c85d71d2d2ca207acc0757
Yannick Marcon <yannick.marcon@obiba.org>
2022-02-08 13:11:28 +0100

    obiba/opal#3569 ValueView interface applied to xstream

--- magma-xstream/src/main/java/org/obiba/magma/xstream/converter/ValueTableConverter.java
+++ magma-xstream/src/main/java/org/obiba/magma/xstream/converter/ValueTableConverter.java
@@ -10,17 +10,16 @@
 
 package org.obiba.magma.xstream.converter;
 
-import org.obiba.magma.MagmaEngine;
-import org.obiba.magma.ValueTable;
-import org.obiba.magma.support.ValueTableReference;
-import org.obiba.magma.views.JoinTable;
-import org.obiba.magma.views.View;
-
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import org.obiba.magma.MagmaEngine;
+import org.obiba.magma.ValueTable;
+import org.obiba.magma.ValueView;
+import org.obiba.magma.support.ValueTableReference;
+import org.obiba.magma.views.JoinTable;
 
 /**
  * Used to de/serialize a {@code ValueTable} as a {@code ValueTableReference}.
@@ -29,14 +28,14 @@ public class ValueTableConverter implements Converter {
 
   @Override
   public boolean canConvert(@SuppressWarnings("rawtypes") Class type) {
-    return ValueTable.class.isAssignableFrom(type) && !type.equals(View.class) && !type.equals(JoinTable.class);
+    return ValueTable.class.isAssignableFrom(type) && !ValueView.class.isAssignableFrom(type) && !type.equals(JoinTable.class);
   }
 
   @Override
   public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
     ValueTable valueTable = (ValueTable) source;
     writer.startNode("reference");
-    if(valueTable instanceof ValueTableReference) {
+    if (valueTable instanceof ValueTableReference) {
       writer.setValue(((ValueTableReference) valueTable).getReference());
     } else {
       writer.setValue(valueTable.getDatasource().getName() + "." + valueTable.getName());
